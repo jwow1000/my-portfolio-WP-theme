8dc24dddddd12539b20b20b29f7ad0d27274a4c2
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-01-31 18:36:26 -0500

    add cv page template

---
+++ cv_page.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: CV Page */
+get_header(); ?>
+
+<main class="cv-page">
+    <h2 class="wp-block-heading">Exhibitions</h2>
+    <div class="list-line"></div>
+    <ul class="cv-list-wrapper">
+        <li><span class="cv-date">October, 2019:</span> <span class="cv-list-name">'Every one hundred feet the world changes'.</span> Montagehalle, HBK Braunschweig, DE.</li> 
+        <li><span class="cv-date">May, 2015:</span> <span class="cv-list-name">'Chain Reaction'</span>. Signal Gallery, Brooklyn NY</li> 
+    </ul>
+
+    <h2 class="wp-block-heading">Residencies</h2>
+    <div class="list-line"></div>
+    <ul class="cv-list-wrapper">
+        <li><span class="cv-date">May - Dec, 2019:</span> <span class="cv-list-name">BS Projects.</span> Braunschweig, DE.</li> 
+        <li><span class="cv-date">Feb, 2015:</span> <span class="cv-list-name">IEA: AIR</span>. Alfred University, Alfred NY</li> 
+    </ul>
+</main>
+
+<?php get_footer(); ?>
